3641352f5c0ff7697c61f70a131ed7c5444d7384
Benson1198 <bensonpx1198@gmail.com>
2026-03-23 00:11:15 +0530

    service layer changes

--- src/main/java/com/urlshortener/dto/UrlResponse.java
+++ src/main/java/com/urlshortener/dto/UrlResponse.java
@@ -6,6 +6,7 @@ import lombok.Data;
 @Data
 @Builder
 public class UrlResponse {
+
     private String shortCode;
     private String originalUrl;
     private String shortUrl;
--- src/main/java/com/urlshortener/service/UrlService.java
+++ src/main/java/com/urlshortener/service/UrlService.java
@@ -1,10 +1,14 @@
 package com.urlshortener.service;
 
+import com.urlshortener.dto.ShortenRequest;
 import com.urlshortener.model.Url;
 import com.urlshortener.repository.UrlRepository;
 import com.urlshortener.util.Base62Encoder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import javax.management.RuntimeErrorException;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,19 +19,38 @@ public class UrlService {
     private final UrlRepository urlRepository;
     private final Base62Encoder base62Encoder;
 
-    public Url shorten(String originalUrl) {
-        Url url = Url.builder() // Save with empty short code first to get DB-generated ID
-                .originalUrl(originalUrl)
-                .shortCode("temp")
-                .build();
+    public UrlResponse shorten(ShortenRequest request) {
+        // Check if custom alias is already taken
+        if (request.getCustomAlias() != null && !request.getCustomAlias().isBlank()) {
+            if (urlRepository.existsByCustomAlias(request.getCustomAlias())) {
+                throw new RuntimeException("Alias already exists: " + request.getCustomAlias());
+            }
+        }
 
+        Url url = Url.builder()
+                .originalUrl(request.getOriginalUrl())
+                .shortCode("temp")
+                .customAlias(request.getCustomAlias())
+                .build();
         url = urlRepository.save(url);
 
-        String shortCode = base62Encoder.encode(url.getId()); // Use the DB ID to generate short code
-
-        url.setShortCode(shortCode); // Update the record with the real short code
-        return urlRepository.save(url);
     }
+
+    // public Url shorten(String originalUrl) {
+    // Url url = Url.builder() // Save with empty short code first to get
+    // DB-generated ID
+    // .originalUrl(originalUrl)
+    // .shortCode("temp")
+    // .build();
+
+    // url = urlRepository.save(url);
+
+    // String shortCode = base62Encoder.encode(url.getId()); // Use the DB ID to
+    // generate short code
+
+    // url.setShortCode(shortCode); // Update the record with the real short code
+    // return urlRepository.save(url);
+    // }
 
     public Url resolve(String shortCode) {
         return urlRepository.findByShortCode(shortCode)
